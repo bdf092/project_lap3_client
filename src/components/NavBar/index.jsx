a150290720d6b227a3171eef74214c5a76c5754e
DuncanCaplan <duncan.caplan@outlook.com>
2023-09-11 12:23:26 +0100

    NavBar with logo

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -5,6 +5,10 @@ const NavBar = () => {
     return (
         <>
             <nav className="navbar">
+                <div className="logo">
+                    <img src="../../../public/images/logo.png" alt="Think Fast Logo" />
+                </div>
+
                 <div className="links">
                     <NavLink to="/">Quizzes</NavLink>
                     <NavLink to="/profile">Profile</NavLink>
