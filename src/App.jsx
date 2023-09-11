62d99c293783707dac4950e33489dbed8c602896
DuncanCaplan <duncan.caplan@outlook.com>
2023-09-11 12:36:26 +0100

    sign up page 1st commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { NavBar } from "./components";
+import { SignUpPage } from "./pages";
 
 const App = () => {
     return (
         <div className="App">
             <Routes>
-                <Route path="/" element={<NavBar />}></Route>
+                <Route path="/" element={<NavBar />}>
+                    <Route path="/signup" element={<SignUpPage />} />
+                </Route>
             </Routes>
         </div>
     );
